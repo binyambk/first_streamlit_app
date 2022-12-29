f1a01e40db40a9561139fb60857798e7bf9804e8
binyambk <100790914+binyambk@users.noreply.github.com>
2022-12-29 09:09:21 -0500

    Cleaned up the presentation of data from the api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,4 +23,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+# streamlit.text(fruityvice_response.json())
+
+# Normalize the data from Fruityvice api
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Display fruit list from API call in a table
+streamlit.dataframe(fruityvice_normalized)
